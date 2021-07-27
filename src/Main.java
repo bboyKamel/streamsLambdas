ec292a2c2f029a37a0362d8dd6fd8cba1a19740e
bboyKamel <nacoon@tlen.pl>
2021-07-27 08:03:00 +0200

    create repo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -6,14 +7,22 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
 
-
+        System.out.println(countElements(myRepo()));
 
     }
 
 
-    public Map<String, Long> countElements(List<String> toCountList) {
+    public static Map<String, Long> countElements(List<String> toCountList) {
         return toCountList.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    public static List<String> myRepo(){
+        List<String> exStrList = new ArrayList<>();
+            exStrList.add("wacław");
+            exStrList.add("rusłan");
+            exStrList.add("tobiasz");
+            exStrList.add("rusłan");
+        return exStrList;
     }
 }
